a8784914c61ee60e3ec2a12561b6734ce6a8cfd9
mayrajp <may.musique@gmail.com>
2022-12-20 12:41:09 -0300

    refactor dashboard to simple template

--- app/src/Controller/DashboardController.php
+++ app/src/Controller/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/dashboard', name: 'dashboard_', methods: ['GET'])]
+#[Route('/admin/dashboard', name: 'admin_dashboard_', methods: ['GET'])]
 class DashboardController extends AbstractController
 {
     public function __construct()
